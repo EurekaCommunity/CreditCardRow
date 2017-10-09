25fd9f7804455ae6b6c601b66a7303992c0a3a0c
pera <santiagofm@xmartlabs.com>
2017-10-09 15:28:45 -0300

    Adds compatibility with Xcode 9 (Swift 4).

--- Sources/String+CreditCardRow.swift
+++ Sources/String+CreditCardRow.swift
@@ -21,7 +21,7 @@ public extension String {
     subscript (r: Range<Int>) -> String {
         let start = characters.index(startIndex, offsetBy: r.lowerBound)
         let end = characters.index(start, offsetBy: r.upperBound - r.lowerBound)
-        return self[Range(start ..< end)]
+        return String(self[Range(start ..< end)])
     }
 
     var length: Int {
